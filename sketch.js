cff542a12cd2af1e23fa0577ea1c7ec55fc35d95
Tor-Fredrik <tori.torgersen@hotmail.com>
2019-09-20 12:50:24 +1000

    ENEMYTEST2

--- sketch.js
+++ sketch.js
@@ -410,10 +410,10 @@ function drawEnemies(){
     
     for(var i = 0; i<8+(level); i++){
       if(i >= 9){
-        enemy1 = createSprite(220+i*70, windowHeight/10-100);
+        enemy1 = createSprite(i*70, windowHeight/10-100);
         enemy1.scale = 0.03;
         enemy1.addImage(enemy1Image);
-        enemy1.attractionPoint(1,450+i*70, windowHeight);
+        enemy1.attractionPoint(1,i*70, windowHeight);
 
       }else {
         enemy1 = createSprite(420+i*70, windowHeight/10);
